83fb721a5bdd9cdeaa21970914fe42031fbc6738
Sherryll Elliott <sherryll@breakroom.cc>
2022-04-04 16:01:43 +0100

    first commit

---
+++ light/lib/light.ex
@@ -0,0 +1,18 @@
+defmodule Light do
+  @moduledoc """
+  Documentation for `Light`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Light.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
